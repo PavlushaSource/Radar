b989dbf1070af6511ae05905e14b346ba0798e9d
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-11-14 19:36:25 +0300

    feat: Add reset function in main menu

--- internal/UI/config.go
+++ internal/UI/config.go
@@ -5,33 +5,85 @@ import (
 	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	"github.com/PavlushaSource/Radar/internal/config"
+	"github.com/PavlushaSource/Radar/internal/utils"
 )
 
 func CreateConfigChoiceFunction(w1, w2 fyne.Window, configBack *config.BackendConfig, configUI *config.UIConfig, ctx context.Context) func() fyne.CanvasObject {
 	return func() fyne.CanvasObject {
 
 		catCount := widget.NewEntry()
-		catCount.PlaceHolder = fmt.Sprintf("%d", configBack.CountCats)
+		catDefaultValue := fmt.Sprintf("%d", configBack.CountCats)
+		catCount.SetText(catDefaultValue)
 
 		updateTime := widget.NewEntry()
-		updateTime.PlaceHolder = "1.5"
+		updateTimeDefaultValue := "0.5"
+		updateTime.SetText(updateTimeDefaultValue)
+
+		angryRadius := widget.NewEntry()
+		angryRadiusDefaultValue := "100"
+		angryRadius.SetText(angryRadiusDefaultValue)
+
+		selectDistanceType := widget.NewSelect([]string{"Euclidean", "Manhattan", "Curvilinear"}, func(s string) {
+			switch s {
+			case "Euclidean":
+				configBack.DistanceType = config.Euclidean
+			case "Manhattan":
+				configBack.DistanceType = config.Manhattan
+			case "Curvilinear":
+				configBack.DistanceType = config.Curvilinear
+			}
+		})
+		selectDistanceType.PlaceHolder = ""
+		selectDistanceTypeDefaultValue := "Euclidean"
+		selectDistanceType.SetSelected(selectDistanceTypeDefaultValue)
 
 		form := &widget.Form{
 			Items: []*widget.FormItem{
 				{Text: "Count cats", Widget: catCount, HintText: "enter a positive number up to 5 * 10^4"},
 				{Text: "Update time", Widget: updateTime, HintText: "time in second"},
+				{Text: "Angry radius", Widget: angryRadius, HintText: "radius in pixels"},
+				{Text: "Distance type", Widget: selectDistanceType},
 			},
 			OnCancel: func() {
+				catCount.SetText(catDefaultValue)
+				updateTime.SetText(updateTimeDefaultValue)
+				angryRadius.SetText(angryRadiusDefaultValue)
+				selectDistanceType.SetSelected(selectDistanceTypeDefaultValue)
 
 			},
 			OnSubmit: func() {
+				var resErr error
 
+				resErr = utils.SaveError(resErr, configBack.SetCountCats(catCount.Text))
+				resErr = utils.SaveError(resErr, configBack.SetUpdateTime(updateTime.Text))
+				resErr = utils.SaveError(resErr, configBack.SetAngryRadius(angryRadius.Text))
+
+				if resErr != nil {
+					dialog.ShowError(resErr, w1)
+					fmt.Println(resErr)
+				} else {
+					fmt.Println("No errors in config")
+					//w1.SetFullScreen(false)
+					//configUI.FullScreenMode = false
+					//w1.Hide()
+					//
+					//w2.SetMaster()
+					//w2.CenterOnScreen()
+					//w2.Resize(configUI.WindowSize)
+					//w2.Show()
+					//
+					//configUI.InMainMenu = false
+				}
 			},
 			SubmitText: "Run",
 			CancelText: "Reset",
 		}
+
+		//form.Append("Distance type", selectDistanceType)
+
 		runButton := widget.NewButton("Run", func() {
 			w1.SetFullScreen(false)
 			configUI.FullScreenMode = false
@@ -47,18 +99,6 @@ func CreateConfigChoiceFunction(w1, w2 fyne.Window, configBack *config.BackendCo
 
 		// TODO Delete this
 		_ = runButton
-
-		selectDistanceType := widget.NewSelect([]string{"Euclidean", "Manhattan", "Curvilinear"}, func(s string) {
-			switch s {
-			case "Euclidean":
-				configBack.DistanceType = config.Euclidean
-			case "Manhattan":
-				configBack.DistanceType = config.Manhattan
-			case "Curvilinear":
-				configBack.DistanceType = config.Curvilinear
-			}
-		})
-		selectDistanceType.PlaceHolder = "Distance type"
 
 		return container.NewCenter(form)
 	}
--- internal/config/backendConfig.go
+++ internal/config/backendConfig.go
@@ -1,6 +1,10 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type DistanceType uint8
 
@@ -20,9 +24,50 @@ type BackendConfig struct {
 
 func NewBackendConfig() BackendConfig {
 	return BackendConfig{
-		DistanceType: SkipZero,
-		UpdateTime:   time.Second * 1,
+		DistanceType: Euclidean,
+		UpdateTime:   time.Millisecond * 500,
 		CountCats:    25,
 		AngryRadius:  100,
 	}
+}
+
+func (c *BackendConfig) SetCountCats(s string) error {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return fmt.Errorf("invalid convert count %s to integer: %w", s, err)
+	}
+
+	if n <= 0 || n > 5*1e5 {
+		return fmt.Errorf("boundaries cat must be 1 <= count <= 5 * 10^5, but got %d", n)
+	}
+
+	c.CountCats = n
+	return nil
+}
+
+func (c *BackendConfig) SetUpdateTime(s string) error {
+	updateTime, err := time.ParseDuration(s + "s")
+	if err != nil {
+		return fmt.Errorf("invalid update time %s: %w", s, err)
+	}
+
+	if updateTime.Milliseconds() < (250 * time.Millisecond).Milliseconds() {
+		return fmt.Errorf("time must be greater than or eqaul to 250ms")
+	}
+
+	c.UpdateTime = updateTime
+	return nil
+}
+
+func (c *BackendConfig) SetAngryRadius(s string) error {
+	r, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return fmt.Errorf("invalid convert radius %s to float: %w", s, err)
+	}
+	if r <= 0 {
+		return fmt.Errorf("radius must be greater than zero")
+	}
+
+	c.AngryRadius = r
+	return nil
 }
