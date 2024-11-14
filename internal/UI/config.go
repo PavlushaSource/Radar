663495c3cf9f1329be2c588833c02ce2b04848c8
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-11-14 14:12:52 +0300

    refactor: Arch

--- internal/UI/config.go
+++ internal/UI/config.go
@@ -2,6 +2,7 @@ package UI
 
 import (
 	"context"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -10,6 +11,27 @@ import (
 
 func CreateConfigChoiceFunction(w1, w2 fyne.Window, configBack *config.BackendConfig, configUI *config.UIConfig, ctx context.Context) func() fyne.CanvasObject {
 	return func() fyne.CanvasObject {
+
+		catCount := widget.NewEntry()
+		catCount.PlaceHolder = fmt.Sprintf("%d", configBack.CountCats)
+
+		updateTime := widget.NewEntry()
+		updateTime.PlaceHolder = "1.5"
+
+		form := &widget.Form{
+			Items: []*widget.FormItem{
+				{Text: "Count cats", Widget: catCount, HintText: "enter a positive number up to 5 * 10^4"},
+				{Text: "Update time", Widget: updateTime, HintText: "time in second"},
+			},
+			OnCancel: func() {
+
+			},
+			OnSubmit: func() {
+
+			},
+			SubmitText: "Run",
+			CancelText: "Reset",
+		}
 		runButton := widget.NewButton("Run", func() {
 			w1.SetFullScreen(false)
 			configUI.FullScreenMode = false
@@ -23,6 +45,21 @@ func CreateConfigChoiceFunction(w1, w2 fyne.Window, configBack *config.BackendCo
 			configUI.InMainMenu = false
 		})
 
-		return container.NewCenter(runButton)
+		// TODO Delete this
+		_ = runButton
+
+		selectDistanceType := widget.NewSelect([]string{"Euclidean", "Manhattan", "Curvilinear"}, func(s string) {
+			switch s {
+			case "Euclidean":
+				configBack.DistanceType = config.Euclidean
+			case "Manhattan":
+				configBack.DistanceType = config.Manhattan
+			case "Curvilinear":
+				configBack.DistanceType = config.Curvilinear
+			}
+		})
+		selectDistanceType.PlaceHolder = "Distance type"
+
+		return container.NewCenter(form)
 	}
 }
