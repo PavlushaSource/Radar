f76654a9227fb2f6f2566c5db402ac1c5d0addb8
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-17 13:35:25 +0300

    Actialize engine stand.

--- cmd/engine/engine.go
+++ cmd/engine/engine.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/PavlushaSource/Radar/model/engine"
 	"github.com/PavlushaSource/Radar/model/geom"
-	"time"
 )
 
 func main() {
 
-	g := geom.NewSimpleGeom(1080, 1920, nil, geom.EuclideanDistance)
-	eng := engine.NewEngine(15, 30, 100, g)
+	g := geom.NewSimpleGeom(1080, 1920, make([]geom.Barrier, 0), geom.EuclideanDistance)
+	eng := engine.NewEngine(15, 30, 50000, g)
 
 	start := time.Now()
 	eng.Run()
