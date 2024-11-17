ea96b18eb9f124c881e0a7236d7741008cfc8a57
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-17 06:55:54 +0300

    Add Copy method in Point entity.

--- model/geom/point.go
+++ model/geom/point.go
@@ -3,6 +3,7 @@ package geom
 type Point interface {
 	X() float64
 	Y() float64
+	Copy() Point
 	set(x float64, y float64)
 }
 
@@ -17,6 +18,13 @@ func (point *point) X() float64 {
 
 func (point *point) Y() float64 {
 	return point.y
+}
+
+func (original *point) Copy() Point {
+	clon := new(point)
+	clon.x = original.x
+	clon.y = original.y
+	return clon
 }
 
 func (point *point) set(x float64, y float64) {
