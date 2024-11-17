8c5ce27b7e573b54a238b71c3e541c2f45f882db
Kirill Shishin <shishin_ka@mail.ru>
2024-11-17 05:17:29 +0300

    chore: Small refactor

--- model/geom/barrier.go
+++ model/geom/barrier.go
@@ -1,5 +1,31 @@
 package geom
 
-type Barrier struct {
-	LineSegment LineSegment
+type Barrier interface {
+	StartPoint() Point
+	FinishPoint() Point
+	set(start Point, finish Point)
+}
+
+type barrier struct {
+	startPoint  Point
+	finishPoint Point
+}
+
+func (barrier *barrier) StartPoint() Point {
+	return barrier.startPoint
+}
+
+func (barrier *barrier) FinishPoint() Point {
+	return barrier.finishPoint
+}
+
+func (barrier *barrier) set(start Point, finish Point) {
+	barrier.startPoint = start
+	barrier.finishPoint = finish
+}
+
+func NewBarrier(start Point, finish Point) Barrier {
+	barrier := new(barrier)
+	barrier.set(start, finish)
+	return barrier
 }
