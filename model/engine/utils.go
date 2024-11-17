74d601d9a80660058a85b3df233955fa46f58dd1
Kirill Shishin <shishin_ka@mail.ru>
2024-11-17 23:26:43 +0300

    chore: Delete extra method

--- model/engine/utils.go
+++ model/engine/utils.go
@@ -16,11 +16,3 @@ func hissingProbability(dist float64) float64 {
 	}
 	return 1.0 - math.Sqrt(1.0-(1.0/(dist*dist)))
 }
-
-func (engine *engine) catsPairIdx(first int64, second int64) int64 {
-	if first <= second {
-		return first*engine.state.NumCats() + second
-	} else {
-		return second*engine.state.NumCats() + first
-	}
-}
