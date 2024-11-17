3b2b5f43e15a57c3d5a18ee07db4e3d0acad7c78
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-17 16:07:44 +0300

    Update Copy and clean logic in engine.Cat.

--- model/engine/cat.go
+++ model/engine/cat.go
@@ -16,110 +16,122 @@ type Cat interface {
 	Status() Status
 	X() float64
 	Y() float64
-	Point() geom.Point
-	Hissings() []int64
-	Fightings() []int64
-	Copy() Cat
+
+	Hissings() []int
+	Fightings() []int
+
+	Copy(dst Cat)
+
+	point() geom.Point
 
 	setStatus(status Status)
 
-	isHissing() bool
+	hissing() bool
 	setHissing(hissing bool)
 
-	setHissings(hissings []int64)
-	setFightings(fightings []int64)
+	addHissing(hissingCat int)
+	addFighting(fightingCat int)
+
+	copyHissingsFrom(src []int)
+	copyFightingsFrom(src []int)
 
 	clean()
 }
 
 type cat struct {
-	status    Status
-	hissing   bool
-	hissings  []int64
-	fightings []int64
-	point     geom.Point
+	_status    Status
+	_hissing   bool
+	_hissings  []int
+	_fightings []int
+	_point     geom.Point
 }
 
 func (cat *cat) Status() Status {
-	return cat.status
+	return cat._status
 }
 
 func (cat *cat) X() float64 {
-	return cat.point.X()
+	return cat._point.X()
 }
 
 func (cat *cat) Y() float64 {
-	return cat.point.Y()
+	return cat._point.Y()
 }
 
-func (cat *cat) Point() geom.Point {
-	return cat.point
+func (cat *cat) Hissings() []int {
+	return cat._hissings
 }
 
-func (cat *cat) Hissings() []int64 {
-	return cat.hissings
+func (cat *cat) Fightings() []int {
+	return cat._fightings
 }
 
-func (cat *cat) Fightings() []int64 {
-	return cat.fightings
+func (cat *cat) Copy(dst Cat) {
+	dst.setStatus(cat._status)
+	dst.setHissing(cat._hissing)
+	dst.copyHissingsFrom(cat._hissings)
+	dst.copyFightingsFrom(cat._fightings)
+
+	cat._point.Copy(dst.point())
 }
 
-func (original *cat) Copy() Cat {
-	copyCat := new(cat)
+//////////////////
 
-	copyCat.status = original.status
-	copyCat.hissing = original.hissing
-	copyCat.point = original.point.Copy()
-
-	copyCat.hissings = make([]int64, 0, len(original.hissings))
-	copy(copyCat.hissings, original.hissings)
-
-	copyCat.fightings = make([]int64, 0, len(original.fightings))
-	copy(copyCat.fightings, original.fightings)
-
-	return copyCat
+func (cat *cat) point() geom.Point {
+	return cat._point
 }
 
 func (cat *cat) setStatus(status Status) {
-	cat.status = status
+	cat._status = status
 }
 
-func (cat *cat) isHissing() bool {
-	return cat.hissing
+func (cat *cat) hissing() bool {
+	return cat._hissing
 }
 
 func (cat *cat) setHissing(hissing bool) {
-	cat.hissing = hissing
+	cat._hissing = hissing
 }
 
-func (cat *cat) setHissings(hissings []int64) {
-	cat.hissings = hissings
+func (cat *cat) addHissing(hissingCat int) {
+	cat._hissings = append(cat._hissings, hissingCat)
 }
 
-func (cat *cat) setFightings(fightings []int64) {
-	cat.fightings = fightings
+func (cat *cat) addFighting(fightingCat int) {
+	cat._fightings = append(cat._fightings, fightingCat)
+}
+
+func (cat *cat) copyHissingsFrom(src []int) {
+	cat._hissings = cat._hissings[:len(src)]
+
+	copy(cat._hissings, src)
+}
+
+func (cat *cat) copyFightingsFrom(src []int) {
+	cat._fightings = cat._fightings[:len(src)]
+
+	copy(cat._fightings, src)
 }
 
 func (cat *cat) clean() {
-	cat.status = Calm
-	cat.hissing = false
-
-	capacity := cap(cat.hissings)
-	cat.hissings = make([]int64, 0, capacity)
-	capacity = cap(cat.fightings)
-	cat.fightings = make([]int64, 0, capacity)
+	cat._status = Calm
+	cat._hissing = false
+	cat._hissings = cat._hissings[:0]
+	cat._fightings = cat._fightings[:0]
 }
+
+//////////////////
 
 func newCat(point geom.Point, numCats int64) Cat {
 	cat := new(cat)
 
-	cat.status = Calm
-	cat.hissing = false
+	cat._status = Calm
+	cat._hissing = false
 
-	cat.hissings = make([]int64, 0, numCats)
-	cat.fightings = make([]int64, 0, numCats)
+	cat._hissings = make([]int, 0, numCats)
+	cat._fightings = make([]int, 0, numCats)
 
-	cat.point = point
+	cat._point = point
 
 	return cat
 }
