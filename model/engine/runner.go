98c57f543f9c1a6f36855d88415876a487125f6e
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-11-17 11:35:39 +0300

    feat: Add runner

---
+++ model/engine/runner.go
@@ -0,0 +1,39 @@
+package engine
+
+import (
+	"context"
+)
+
+type Runnable interface {
+	Run(ctx context.Context) <-chan State
+}
+
+type Runner struct {
+	bufferSize int64
+	engine     Engine
+}
+
+func CreateRunner(engine Engine, bufferSize int64) Runnable {
+	return &Runner{
+		engine:     engine,
+		bufferSize: bufferSize,
+	}
+}
+
+func (r *Runner) Run(ctx context.Context) <-chan State {
+	resCh := make(chan State, r.bufferSize)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(resCh)
+				return
+			default:
+				resCh <- r.engine.Run()
+			}
+		}
+	}()
+
+	return resCh
+}
