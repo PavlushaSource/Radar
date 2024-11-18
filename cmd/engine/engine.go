78acd83543c64cc52f31153d5109af6f8178956e
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-18 17:27:56 +0300

    Sample engine run.

--- cmd/engine/engine.go
+++ cmd/engine/engine.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
-	"runtime/pprof"
 	"time"
 
+	"github.com/PavlushaSource/Radar/model/core/rnd"
 	"github.com/PavlushaSource/Radar/model/engine"
 	"github.com/PavlushaSource/Radar/model/geom"
 )
@@ -17,15 +18,32 @@ func main() {
 	}
 	defer f.Close()
 
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
+	rndAsync := rnd.NewRndCore()
 
-	g := geom.NewSimpleGeom(1080, 1920, make([]geom.Barrier, 0), geom.EuclideanDistance)
-	eng := engine.NewEngine(15, 30, 50000, g)
+	g := geom.NewSimpleGeom(1080, 1920, make([]geom.Barrier, 0), geom.EuclideanDistance, rndAsync)
+	eng := engine.NewEngine(1, 2, 50000, g, rndAsync, 5)
+	getCh, putCh := eng.Run(context.Background())
 
-	start := time.Now()
-	eng.Run()
-	end := time.Now()
-	fmt.Println("Calculate time", end.Sub(start))
+	// start := time.Now()
+	// state := <-getCh
+	// fmt.Println(state.NumCats())
+	// end := time.Now()
+	// fmt.Println("Calculation time:", end.Sub(start))
+
+	for {
+		start := time.Now()
+		state := <-getCh
+		fmt.Println(state.NumCats())
+		putCh <- state
+		end := time.Now()
+		fmt.Println("Calculation time:", end.Sub(start))
+	}
+
+	// start := time.Now()
+	// pprof.StartCPUProfile(f)
+	// eng.Run(context.Background())
+	// pprof.StopCPUProfile()
+	// end := time.Now()
+	// fmt.Println("Calculate time", end.Sub(start))
 
 }
