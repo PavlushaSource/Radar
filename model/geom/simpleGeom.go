004103feb316ef1b42beec96ff2031f45c7d1894
Kirill Shishin <shishin_ka@mail.ru>
2024-11-19 00:50:50 +0300

    feat: Add Point regenerating when point are not achievable

--- model/geom/simpleGeom.go
+++ model/geom/simpleGeom.go
@@ -16,12 +16,25 @@ func (geom *simpleGeom) MovePoint(point Point) {
 	y := geom.rndAsync.Float64ByFloat64(point.Y()) * geom.height
 
 	movedPoint := geom.LimitPointMovement(point, NewPoint(x, y))
+	movedPoint = geom.CorrectMovingAchievable(point, movedPoint, geom.newSimpleRandomPoint)
 
 	point.set(movedPoint.X(), movedPoint.Y())
 }
 
-func NewSimpleGeom(height float64, width float64, barriers []Barrier, maxMoveDistance float64, distance Distance, rndAsync rnd.RndAsync) Geom {
+func (geom *simpleGeom) newSimpleRandomPoint(_ Point) Point {
+	return geom.NewRandomPoint()
+}
+
+func NewSimpleGeom(
+	height float64,
+	width float64,
+	barriers []Barrier,
+	maxMoveDistance float64,
+	distance Distance,
+	rndAsync rnd.RndAsync,
+) Geom {
 	geom := new(simpleGeom)
+
 	geom.height = height
 	geom.width = width
 	geom.barriers = barriers
@@ -29,5 +42,6 @@ func NewSimpleGeom(height float64, width float64, barriers []Barrier, maxMoveDis
 	geom.maxMoveDistance = maxMoveDistance
 	geom.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	geom.rndAsync = rndAsync
+
 	return geom
 }
--- model/geom/vectorGeom.go
+++ model/geom/vectorGeom.go
@@ -8,15 +8,17 @@ import (
 	"github.com/PavlushaSource/Radar/model/core/rnd"
 )
 
-const maxPointMovingDegree = 359
+const fullCircleDegree = 360
 
 type vectorGeom struct {
 	baseGeom
 }
 
 func (geom *vectorGeom) MovePoint(point Point) {
-	distance := geom.rndAsync.Float64ByFloat64(point.X()) * math.Max(geom.width, geom.width)
-	degree := geom.rndAsync.Float64ByFloat64(point.Y()) * maxPointMovingDegree
+	distance := geom.rndAsync.Float64ByFloat64(point.X())
+
+	rndForDegree := geom.rndAsync.Float64ByFloat64(point.Y())
+	degree := rndForDegree - (rndForDegree/fullCircleDegree)*fullCircleDegree
 
 	radians := degree * math.Pi / 180.0
 
@@ -24,12 +26,33 @@ func (geom *vectorGeom) MovePoint(point Point) {
 	y := math.Max(math.Min(point.Y()+distance*math.Sin(radians), geom.height), 0)
 
 	movedPoint := geom.LimitPointMovement(point, NewPoint(x, y))
+	movedPoint = geom.CorrectMovingAchievable(point, movedPoint, geom.newVectorRandomPoint)
 
 	point.set(movedPoint.X(), movedPoint.Y())
 }
 
-func NewVectorGeom(height float64, width float64, barriers []Barrier, maxMoveDistance float64, distance Distance, rndAsync rnd.RndAsync) Geom {
+func (geom *vectorGeom) newVectorRandomPoint(point Point) Point {
+	distance := geom.rnd.Float64() * math.Max(geom.width, geom.width)
+	degree := geom.rnd.Float64() * (fullCircleDegree - 1)
+
+	radians := degree * math.Pi / 180.0
+
+	x := math.Max(math.Min(point.X()+distance*math.Cos(radians), geom.width), 0)
+	y := math.Max(math.Min(point.Y()+distance*math.Sin(radians), geom.height), 0)
+
+	return NewPoint(x, y)
+}
+
+func NewVectorGeom(
+	height float64,
+	width float64,
+	barriers []Barrier,
+	maxMoveDistance float64,
+	distance Distance,
+	rndAsync rnd.RndAsync,
+) Geom {
 	geom := new(vectorGeom)
+
 	geom.height = height
 	geom.width = width
 	geom.barriers = barriers
@@ -37,5 +60,6 @@ func NewVectorGeom(height float64, width float64, barriers []Barrier, maxMoveDis
 	geom.maxMoveDistance = maxMoveDistance
 	geom.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	geom.rndAsync = rndAsync
+
 	return geom
 }
