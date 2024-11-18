af2ed1310c5d4d3124536e5cbc65d5af3d0c554d
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-18 14:47:51 +0300

    Add asyncRand with precalc to geom.

--- model/geom/simpleGeom.go
+++ model/geom/simpleGeom.go
@@ -3,6 +3,8 @@ package geom
 import (
 	"math/rand"
 	"time"
+
+	"github.com/PavlushaSource/Radar/model/core/rnd"
 )
 
 type simpleGeom struct {
@@ -10,18 +12,19 @@ type simpleGeom struct {
 }
 
 func (geom *simpleGeom) MovePoint(point Point) {
-	x := geom.rng.Float64() * geom.width
-	y := geom.rng.Float64() * geom.height
+	x := geom.rndAsync.Float64ByFloat64(point.X()) * geom.width
+	y := geom.rndAsync.Float64ByFloat64(point.Y()) * geom.height
 
 	point.set(x, y)
 }
 
-func NewSimpleGeom(height float64, width float64, barriers []Barrier, distance Distance) Geom {
+func NewSimpleGeom(height float64, width float64, barriers []Barrier, distance Distance, rndAsync rnd.RndAsync) Geom {
 	geom := new(simpleGeom)
 	geom.height = height
 	geom.width = width
 	geom.barriers = barriers
 	geom.distance = distance
-	geom.rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+	geom.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+	geom.rndAsync = rndAsync
 	return geom
 }
