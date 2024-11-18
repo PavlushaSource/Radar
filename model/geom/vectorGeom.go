62412b01212a90b2bef4f0edf249ea571b45aaf0
Kirill Shishin <shishin_ka@mail.ru>
2024-11-18 22:39:57 +0300

    feat: Add points movements limit

--- model/geom/vectorGeom.go
+++ model/geom/vectorGeom.go
@@ -23,15 +23,18 @@ func (geom *vectorGeom) MovePoint(point Point) {
 	x := math.Max(math.Min(point.X()+distance*math.Cos(radians), geom.width), 0)
 	y := math.Max(math.Min(point.Y()+distance*math.Sin(radians), geom.height), 0)
 
-	point.set(x, y)
+	movedPoint := geom.LimitPointMovement(point, NewPoint(x, y))
+
+	point.set(movedPoint.X(), movedPoint.Y())
 }
 
-func NewVectorGeom(height float64, width float64, barriers []Barrier, distance Distance, rndAsync rnd.RndAsync) Geom {
+func NewVectorGeom(height float64, width float64, barriers []Barrier, maxMoveDistance float64, distance Distance, rndAsync rnd.RndAsync) Geom {
 	geom := new(vectorGeom)
 	geom.height = height
 	geom.width = width
 	geom.barriers = barriers
 	geom.distance = distance
+	geom.maxMoveDistance = maxMoveDistance
 	geom.rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	geom.rndAsync = rndAsync
 	return geom
