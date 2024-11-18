88eeb5a5e33d876afd333d2949c5fdee6bd4e452
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-18 05:36:01 +0300

    Move processing logic to Processor.

--- model/engine/engine.go
+++ model/engine/engine.go
@@ -1,109 +1,27 @@
 package engine
 
 import (
-	"math/rand"
-	"sync"
-
 	"github.com/PavlushaSource/Radar/model/geom"
 )
 
 type engine struct {
-	state *state
-
-	geom   geom.Geom
-	radius float64
-
-	rndCores []*rand.Rand
-
-	numColumns int
-	numRows    int
-	cells      [][]int
-
-	hissings [][]bool
+	processor *processor
+	state     *state
 }
 
 func (engine *engine) Run() State {
-	engine.clean()
 
-	engine.moveCats()
-	engine.processCatsNeighbours()
+	engine.processor.process(engine.state)
 
-	// return engine.state.Copy()
 	return engine.state
 }
 
 func NewEngine(radiusFight float64, radiusHiss float64, numCats int, geom geom.Geom) Engine {
-	var wg sync.WaitGroup
-
 	engine := new(engine)
 
-	engine.geom = geom
 	engine.state = newState(geom.Height(), geom.Width(), radiusFight, radiusHiss, numCats, geom)
-	engine.radius = radiusHiss
 
-	engine.rndCores = make([]*rand.Rand, numCats)
-	for i := range engine.rndCores {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			engine.rndCores[i] = newRndCore()
-		}()
-	}
+	engine.processor = newProcessor(radiusHiss, numCats, geom)
 
-	engine.numColumns = numColumns(geom.Width(), radiusHiss)
-	engine.numRows = numRows(geom.Height(), radiusHiss)
-	numCells := engine.numColumns * engine.numRows
-	engine.cells = make([][]int, numCells)
-	for i := range engine.cells {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			engine.cells[i] = make([]int, 0, numCats)
-		}()
-	}
-
-	engine.hissings = make([][]bool, numCats)
-	for i := range engine.hissings {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			engine.hissings[i] = make([]bool, numCats)
-		}()
-	}
-
-	wg.Wait()
 	return engine
-}
-
-func (engine *engine) clean() {
-	var wg sync.WaitGroup
-
-	for i := range engine.cells {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			engine.cells[i] = engine.cells[i][:0]
-		}()
-	}
-
-	engine.hissings = make([][]bool, engine.state.NumCats())
-	for i := range engine.hissings {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			engine.hissings[i] = make([]bool, engine.state.NumCats())
-		}()
-	}
-
-	// runBlocking(
-	// 	&engine.hissings,
-	// 	func(i int, _ []bool) {
-	// 		for j := 0; j < engine.state.NumCats(); j++ {
-	// 			engine.hissings[i][j] = false
-	// 		}
-	// 	})
-
-	engine.state.clean()
-
-	wg.Wait()
 }
--- model/engine/processor.go
+++ model/engine/processor.go
@@ -1,119 +1,261 @@
 package engine
 
-import "sync"
+import (
+	"math/rand"
+	"sync"
 
-func (engine *engine) moveCats() {
+	"github.com/PavlushaSource/Radar/model/geom"
+)
+
+type processor struct {
+	state *state
+
+	geom   geom.Geom
+	radius float64
+
+	rndCores []*rand.Rand
+
+	numColumns int
+	numRows    int
+	cells      [][]int
+
+	points []geom.Point
+
+	hissings [][]bool
+}
+
+func newProcessor(radius float64, numCats int, geometry geom.Geom) *processor {
 	var wg sync.WaitGroup
 
-	for _, cat := range engine.state.cats {
+	processor := new(processor)
+
+	processor.geom = geometry
+
+	processor.points = make([]geom.Point, numCats)
+
+	for i := range processor.points {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			engine.geom.MovePoint(cat._point)
+			processor.points[i] = geometry.NewPoint()
+		}()
+	}
+
+	processor.radius = radius
+
+	processor.rndCores = make([]*rand.Rand, numCats)
+	for i := range processor.rndCores {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.rndCores[i] = newRndCore()
+		}()
+	}
+
+	processor.numColumns = numColumns(geometry.Width(), radius)
+	processor.numRows = numRows(geometry.Height(), radius)
+	numCells := processor.numColumns * processor.numRows
+	processor.cells = make([][]int, numCells)
+	for i := range processor.cells {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.cells[i] = make([]int, 0, numCats)
+		}()
+	}
+
+	processor.hissings = make([][]bool, numCats)
+	for i := range processor.hissings {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.hissings[i] = make([]bool, numCats)
 		}()
 	}
 
 	wg.Wait()
+	return processor
+}
 
-	for i, cat := range engine.state.cats {
+func (processor *processor) process(state *state) *state {
+	processor.state = state
+	processor.setUp()
+
+	processor.moveCats()
+	processor.processCatsNeighbours()
+
+	processor.tearDown()
+
+	return processor.state
+}
+
+func (processor *processor) setUp() {
+	var wg sync.WaitGroup
+
+	for i := range processor.cells {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			cell := engine.cell(cat._point)
-			engine.cells[cell] = append(engine.cells[cell], i)
+			processor.cells[i] = processor.cells[i][:0]
+		}()
+	}
+
+	processor.hissings = make([][]bool, processor.state.NumCats())
+	for i := range processor.hissings {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.hissings[i] = make([]bool, processor.state.NumCats())
+		}()
+	}
+
+	// runBlocking(
+	// 	&processor.hissings,
+	// 	func(i int, _ []bool) {
+	// 		for j := 0; j < processor.state.NumCats(); j++ {
+	// 			processor.hissings[i][j] = false
+	// 		}
+	// 	})
+
+	processor.state.clean()
+
+	for i := range processor.points {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.points[i].Copy(processor.state.cats[i]._point)
 		}()
 	}
 
 	wg.Wait()
 }
 
-func (engine *engine) processCatsNeighbours() {
+func (processor *processor) tearDown() {
 	var wg sync.WaitGroup
 
-	for i := range engine.state.cats {
+	for i := range processor.points {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			engine.processCatNeighbours(i)
+			processor.state.cats[i]._point.Copy(processor.points[i])
+		}()
+	}
+
+	wg.Wait()
+}
+
+func (processor *processor) moveCats() {
+	var wg sync.WaitGroup
+
+	for _, cat := range processor.state.cats {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.geom.MovePoint(cat._point)
 		}()
 	}
 
 	wg.Wait()
 
-	for i, cat := range engine.state.cats {
+	for i, cat := range processor.state.cats {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cell := processor.cell(cat._point)
+			processor.cells[cell] = append(processor.cells[cell], i)
+		}()
+	}
+
+	wg.Wait()
+}
+
+func (processor *processor) processCatsNeighbours() {
+	var wg sync.WaitGroup
+
+	for i := range processor.state.cats {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processor.processCatNeighbours(i)
+		}()
+	}
+
+	wg.Wait()
+
+	for i, cat := range processor.state.cats {
 		wg.Add(2)
 		go func() {
 			defer wg.Done()
-			engine.postprocessCatHissings(i)
+			processor.postprocessCatHissings(i)
 		}()
 
 		go func() {
 			defer wg.Done()
-			engine.updateCatStatus(cat)
+			processor.updateCatStatus(cat)
 		}()
 	}
 
 	wg.Wait()
 }
 
-func (engine *engine) processCatNeighbours(catIdx int) {
-	cat := engine.state.cats[catIdx]
-	cell := engine.cell(cat._point)
+func (processor *processor) processCatNeighbours(catIdx int) {
+	cat := processor.state.cats[catIdx]
+	cell := processor.cell(cat._point)
 
-	engine.processCell(catIdx, cell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetUpCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetDownCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetLeftCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetRightCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetUpLeftCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetUpRightCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetDownLeftCell)
-	engine.processNeighbourCell(catIdx, cell, engine.tryGetDownRightCell)
+	processor.processCell(catIdx, cell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetUpCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetDownCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetLeftCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetRightCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetUpLeftCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetUpRightCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetDownLeftCell)
+	processor.processNeighbourCell(catIdx, cell, processor.tryGetDownRightCell)
 }
 
-func (engine *engine) processNeighbourCell(catIdx int, cell int, tryGetNeighbourCell neighbourCellExtractor) {
+func (processor *processor) processNeighbourCell(catIdx int, cell int, tryGetNeighbourCell neighbourCellExtractor) {
 	if success, neighbourCell := tryGetNeighbourCell(cell); success {
-		engine.processCell(catIdx, neighbourCell)
+		processor.processCell(catIdx, neighbourCell)
 	}
 }
 
-func (engine *engine) processCell(catIdx int, cell int) {
-	for _, neighbour := range engine.cells[cell] {
-		engine.proccessPair(catIdx, neighbour)
+func (processor *processor) processCell(catIdx int, cell int) {
+	for _, neighbour := range processor.cells[cell] {
+		processor.proccessPair(catIdx, neighbour)
 	}
 }
 
-func (engine *engine) proccessPair(catIdx int, neighbourIdx int) {
-	cat := engine.state.cats[catIdx]
-	neighbour := engine.state.cats[neighbourIdx]
-	dist := engine.geom.Distance(cat.point(), neighbour.point())
+func (processor *processor) proccessPair(catIdx int, neighbourIdx int) {
+	cat := processor.state.cats[catIdx]
+	neighbour := processor.state.cats[neighbourIdx]
+	dist := processor.geom.Distance(cat.point(), neighbour.point())
 
-	if dist <= engine.state.radiusFight {
+	if dist <= processor.state.radiusFight {
 		cat.setStatus(Fighting)
 		neighbour.setStatus(Fighting)
 
 		cat.fightings = append(cat.fightings, neighbourIdx)
-	} else if dist <= engine.state.radiusHiss {
-		if engine.rndCores[catIdx].Float64() <= hissingProbability(dist) {
+	} else if dist <= processor.state.radiusHiss {
+		if processor.rndCores[catIdx].Float64() <= hissingProbability(dist) {
 			cat.hissing = true
 			neighbour.hissing = true
 
-			engine.hissings[catIdx][neighbourIdx] = true
+			processor.hissings[catIdx][neighbourIdx] = true
 		}
 
 		cat.hissings = append(cat.hissings, neighbourIdx)
 	}
 }
 
-func (engine *engine) postprocessCatHissings(catIdx int) {
-	cat := engine.state.cats[catIdx]
+func (processor *processor) postprocessCatHissings(catIdx int) {
+	cat := processor.state.cats[catIdx]
 	if !cat.hissing {
 		return
 	}
 
 	n := 0
 	for _, neighbour := range cat.hissings {
-		if engine.hissings[catIdx][neighbour] || engine.hissings[neighbour][catIdx] {
+		if processor.hissings[catIdx][neighbour] || processor.hissings[neighbour][catIdx] {
 			cat.hissings[n] = neighbour
 			n++
 		}
@@ -121,7 +263,7 @@ func (engine *engine) postprocessCatHissings(catIdx int) {
 	cat.hissings = cat.hissings[:n]
 }
 
-func (engine *engine) updateCatStatus(cat *cat) {
+func (processor *processor) updateCatStatus(cat *cat) {
 	if cat.hissing && (cat.status == Calm) {
 		cat.status = Hissing
 	}
