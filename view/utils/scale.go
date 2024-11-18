03740ad439d63895f610bc2220f86903dcd50d80
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-11-18 23:09:10 +0300

    feat: Add border and right rescale

--- view/utils/scale.go
+++ view/utils/scale.go
@@ -1,16 +1,20 @@
 package utils
 
-import "fyne.io/fyne/v2"
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+)
 
 const (
 	scaleRatio = 1.1
-	maxScale   = 2
+	maxScale   = 3
 	minScale   = 0.01
 )
 
 func IncreaseScale(container *fyne.Container, width, height, scale float32) float32 {
 	newScale := min(scale*scaleRatio, maxScale)
 
+	fmt.Println("RESIZE CONTAINER", width, height)
 	updateContainerSize(container, width, height, newScale)
 
 	return newScale
@@ -19,6 +23,7 @@ func IncreaseScale(container *fyne.Container, width, height, scale float32) floa
 func DecreaseScale(container *fyne.Container, width, height, scale float32) float32 {
 	newScale := max(scale/scaleRatio, minScale)
 
+	fmt.Println("RESIZE CONTAINER", width, height)
 	updateContainerSize(container, width, height, newScale)
 
 	return newScale
