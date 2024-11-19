c75711c14996603b923843557e31b6ade73c86ec
Kirill Shishin <shishin_ka@mail.ru>
2024-11-19 10:09:16 +0300

    fix: change sync rand to async

--- model/geom/vectorGeom.go
+++ model/geom/vectorGeom.go
@@ -15,23 +15,19 @@ type vectorGeom struct {
 }
 
 func (geom *vectorGeom) MovePoint(point Point) {
-	distance := geom.rndAsync.Float64ByFloat64(point.X()) * math.Max(geom.width, geom.width)
-	degree := geom.rndAsync.Float64ByFloat64(point.Y()) * maxPointMovingDegree
-
-	radians := degree * math.Pi / 180.0
-
-	x := math.Max(math.Min(point.X()+distance*math.Cos(radians), geom.width), 0)
-	y := math.Max(math.Min(point.Y()+distance*math.Sin(radians), geom.height), 0)
-
-	movedPoint := geom.LimitPointMovement(point, NewPoint(x, y))
+	movedPoint := geom.newVectorRandomPoint(point)
+	movedPoint = geom.LimitPointMovement(point, movedPoint)
 	movedPoint = geom.CorrectMovingAchievable(point, movedPoint, geom.newVectorRandomPoint)
 
 	point.set(movedPoint.X(), movedPoint.Y())
 }
 
 func (geom *vectorGeom) newVectorRandomPoint(point Point) Point {
-	distance := geom.rnd.Float64() * math.Max(geom.width, geom.width)
-	degree := geom.rnd.Float64() * maxPointMovingDegree
+	distance := geom.rndAsync.Float64ByFloat64(point.X()*float64(geom.rndCounter)) * math.Max(geom.width, geom.width)
+	geom.IncrementRndCounter()
+
+	degree := geom.rndAsync.Float64ByFloat64(point.Y()*float64(geom.rndCounter)) * maxPointMovingDegree
+	geom.IncrementRndCounter()
 
 	radians := degree * math.Pi / 180.0
 
