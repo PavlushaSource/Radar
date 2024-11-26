7ca93cf25ab6d4172de4c53aa36a6b5211f62d3b
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-26 15:19:52 +0300

    Add engine/processor docs comments.

--- model/engine/processor.go
+++ model/engine/processor.go
@@ -7,26 +7,46 @@ import (
 	"github.com/PavlushaSource/Radar/model/geom"
 )
 
+// The processor type represents the engine processor and is used for processing while the engine is running.
 type processor struct {
+	// Current state
 	state *State
 
-	numCats     int
-	geom        geom.Geom
+	// Number of cats.
+	numCats int
+	// Using geometry.
+	geom geom.Geom
+	// Fight radius.
 	radiusFight float64
-	radiusHiss  float64
+	// Hiss radius.
+	radiusHiss float64
 
+	// Cell size.
 	cellSize float64
 
+	// Async random used for generating random values from any async job.
 	rndAsync rnd.RndAsync
-	cf       int
+	// coefficient used to parameterize the asynchronous random call.
+	cf int
 
+	// Number of columns in the field.
 	numColumns int
-	numRows    int
-	cells      [][]int
+	// Number of rows in the field.
+	numRows int
+	// Array of cells.
+	//
+	// cell[cellIdx] stores the indeces of cats in the cell with index cellIdx.
+	cells [][]int
 
+	// Point pool used to store cats coordinates between model state processing.
 	points []geom.Point
 }
 
+// newProcessor creates new processor object by
+// fight and hiss radiuses,
+// number of cats,
+// geometry and
+// async random.
 func newProcessor(radiusFight float64, radiusHiss float64, numCats int, geometry geom.Geom, rndAsync rnd.RndAsync) *processor {
 	var wg sync.WaitGroup
 
@@ -70,6 +90,7 @@ func newProcessor(radiusFight float64, radiusHiss float64, numCats int, geometry
 	return processor
 }
 
+// process is a processor method that the given process state.
 func (processor *processor) process(state *State) *State {
 	processor.state = state
 	processor.setUp()
@@ -83,6 +104,8 @@ func (processor *processor) process(state *State) *State {
 	return processor.state
 }
 
+// setUp is a processor method
+// used to configure the processor before processing state.
 func (processor *processor) setUp() {
 	var wg sync.WaitGroup
 
@@ -107,6 +130,8 @@ func (processor *processor) setUp() {
 	wg.Wait()
 }
 
+// tearDown is a processor method
+// used to tune the processor after state has been processed.
 func (processor *processor) tearDown() {
 	var wg sync.WaitGroup
 
@@ -121,6 +146,7 @@ func (processor *processor) tearDown() {
 	wg.Wait()
 }
 
+// moveCats is a processor method that changes the coordinates of cats in model state.
 func (processor *processor) moveCats() {
 	var wg sync.WaitGroup
 
@@ -135,6 +161,7 @@ func (processor *processor) moveCats() {
 	wg.Wait()
 }
 
+// moveCats is a processor method that partitions cats into cells based on their coordinates.
 func (processor *processor) cellSplitting() {
 	for i := range processor.state.cats {
 		if success, cell := processor.tryGetCell(processor.state.cats[i]); success {
@@ -143,6 +170,8 @@ func (processor *processor) cellSplitting() {
 	}
 }
 
+// processCatsNeighbors is a processor method
+// that calculates the new statuses of cats based on their interactions with neighbors.
 func (processor *processor) processCatsNeighbours() {
 	var wg sync.WaitGroup
 
@@ -211,6 +240,9 @@ func (processor *processor) processCatsNeighbours() {
 	wg.Wait()
 }
 
+// processCellWithSelf is a processor method
+// that handles the interaction of cats in a cell
+// based on the given column and row of the cell.
 func (processor *processor) processCellWithSelf(col int, row int) {
 	for _, ci := range processor.cells[processor.cellByRowColumn(row, col)] {
 		for _, ni := range processor.cells[processor.cellByRowColumn(row, col)] {
@@ -222,6 +254,9 @@ func (processor *processor) processCellWithSelf(col int, row int) {
 	}
 }
 
+// processCellWithOther is a processor method
+// that handles the interaction of cats from cell with cats from another cell
+// based on the given columns and rows of the cells.
 func (processor *processor) processCellWithOther(col int, row int, otherCol int, otherRow int) {
 	for _, ci := range processor.cells[processor.cellByRowColumn(row, col)] {
 		for _, ni := range processor.cells[processor.cellByRowColumn(otherRow, otherCol)] {
@@ -230,6 +265,8 @@ func (processor *processor) processCellWithOther(col int, row int, otherCol int,
 	}
 }
 
+// proccessPair is a processor method
+// that handles the interation a cat with each other.
 func (processor *processor) proccessPair(catIdx int, neighbourIdx int) {
 	dist := processor.geom.Distance(processor.state.cats[catIdx], processor.state.cats[neighbourIdx])
 
@@ -244,6 +281,8 @@ func (processor *processor) proccessPair(catIdx int, neighbourIdx int) {
 	}
 }
 
+// updateCatStatus is a processor method
+// that attempts to set the given cat status to Hissing.
 func (processor *processor) updateCatStatus(idx int) {
 	if processor.state.cats[idx].hissing && (processor.state.cats[idx].status == Calm) {
 		processor.state.cats[idx].status = Hissing
