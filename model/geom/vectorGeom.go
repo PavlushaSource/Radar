51346e420fa669026e959309fa7e7d2646e7abc7
Kirill Shishin <shishin_ka@mail.ru>
2024-11-26 23:40:55 +0300

    chore: Add docs to the vector geom

--- model/geom/vectorGeom.go
+++ model/geom/vectorGeom.go
@@ -10,10 +10,13 @@ import (
 
 const maxPointMovingDegree = 359
 
+// vectorGeom is a struct for vector geometry
+// In vectorGeom, the points move straight, deviating by some angle
 type vectorGeom struct {
 	baseGeom
 }
 
+// MovePoint moves the point to other coordinates that meet the constraints of the field
 func (geom *vectorGeom) MovePoint(point Point) {
 	movedPoint := geom.newVectorRandomPoint(point)
 	movedPoint = geom.LimitPointMovement(point, movedPoint)
@@ -22,6 +25,7 @@ func (geom *vectorGeom) MovePoint(point Point) {
 	point.set(movedPoint.X(), movedPoint.Y())
 }
 
+// newVectorRandomPoint generates new random point, taking into account the coordinates of the old point
 func (geom *vectorGeom) newVectorRandomPoint(point Point) Point {
 	distance := geom.rndAsync.Float64ByFloat64(point.X()*float64(geom.rndCounter)) * math.Max(geom.width, geom.width)
 	geom.IncrementRndCounter()
@@ -37,6 +41,7 @@ func (geom *vectorGeom) newVectorRandomPoint(point Point) Point {
 	return NewPoint(x, y)
 }
 
+// NewVectorGeom creates new instance of the vector geometry
 func NewVectorGeom(
 	height float64,
 	width float64,
