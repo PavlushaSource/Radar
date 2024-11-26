178ebd34e6b45b6adf3decb29b998973e6422443
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-11-26 14:24:56 +0300

    Add engine/state docs comments.

--- model/engine/state.go
+++ model/engine/state.go
@@ -6,18 +6,22 @@ import (
 	"github.com/PavlushaSource/Radar/model/geom"
 )
 
+// The state type, which represents the state of the model, contains a pool of cats.
 type State struct {
 	cats []*Cat
 }
 
+// NumCats is a State method that returns the number of cats.
 func (state *State) NumCats() int {
 	return len(state.cats)
 }
 
+// CatsElementAt is a State method that returns the cat by its index.
 func (state *State) CatsElementAt(idx int) *Cat {
 	return state.cats[idx]
 }
 
+// clean is a State method, which cleans each cat in this state.
 func (state *State) clean() {
 	var wg sync.WaitGroup
 
@@ -32,6 +36,7 @@ func (state *State) clean() {
 	wg.Wait()
 }
 
+// newState creates a new model state by number of cats.
 func newState(numCats int, geom geom.Geom) *State {
 	state := new(State)
 
