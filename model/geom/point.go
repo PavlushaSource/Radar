1928c872607ec751bfb98476f2ab3abac7bb33ca
Kirill Shishin <shishin_ka@mail.ru>
2024-11-26 23:49:16 +0300

    chore: Add docs to the Point

--- model/geom/point.go
+++ model/geom/point.go
@@ -1,5 +1,6 @@
 package geom
 
+// Point is a type interface represents point on the 2d field
 type Point interface {
 	X() float64
 	Y() float64
@@ -7,6 +8,7 @@ type Point interface {
 	set(x float64, y float64)
 }
 
+// point is a struct represents point on the 2d field
 type point struct {
 	x float64
 	y float64
@@ -20,15 +22,18 @@ func (point *point) Y() float64 {
 	return point.y
 }
 
+// Copy set this point coordinates to dst point
 func (point *point) Copy(dst Point) {
 	dst.set(point.x, point.y)
 }
 
+// set new coordinates to the point
 func (point *point) set(x float64, y float64) {
 	point.x = x
 	point.y = y
 }
 
+// NewPoint returns new instance of the Point
 func NewPoint(x float64, y float64) Point {
 	point := new(point)
 	point.set(x, y)
