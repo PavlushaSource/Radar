548455432a3b3d8668b429ab2270c0987500006a
Kirill Shishin <shishin_ka@mail.ru>
2024-11-26 23:28:59 +0300

    chore: Add docs to the simple geom

--- model/geom/simpleGeom.go
+++ model/geom/simpleGeom.go
@@ -7,10 +7,13 @@ import (
 	"github.com/PavlushaSource/Radar/model/core/rnd"
 )
 
+// simpleGeom is a struct for simple geometry
+// In simpleGeom, the points can move to a random point
 type simpleGeom struct {
 	baseGeom
 }
 
+// MovePoint moves the point to other coordinates that meet the constraints of the field
 func (geom *simpleGeom) MovePoint(point Point) {
 	movedPoint := geom.newSimpleRandomPoint(point)
 	movedPoint = geom.LimitPointMovement(point, movedPoint)
@@ -19,6 +22,7 @@ func (geom *simpleGeom) MovePoint(point Point) {
 	point.set(movedPoint.X(), movedPoint.Y())
 }
 
+// newSimpleRandomPoint generates new random point, taking into account the coordinates of the old point
 func (geom *simpleGeom) newSimpleRandomPoint(point Point) Point {
 	x := geom.rndAsync.Float64ByFloat64(point.X()*float64(geom.rndCounter)) * geom.width
 	geom.IncrementRndCounter()
@@ -29,6 +33,7 @@ func (geom *simpleGeom) newSimpleRandomPoint(point Point) Point {
 	return NewPoint(x, y)
 }
 
+// NewSimpleGeom creates new instance of the simple geometry
 func NewSimpleGeom(
 	height float64,
 	width float64,
