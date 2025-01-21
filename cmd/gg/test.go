c998ea0a572957fb8926cb899f8ec9aee93fa7c0
PavlushaSource <pavlushkin.dima1785@gmail.com>
2025-01-21 08:54:11 +0300

    feat: Add MainMenu without integration

--- cmd/gg/test.go
+++ cmd/gg/test.go
@@ -3,11 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"image/color"
-	"log"
-	"strings"
-	"unicode/utf8"
-
 	"github.com/ebitenui/ebitenui"
 	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
@@ -15,117 +10,101 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"golang.org/x/image/font/gofont/goregular"
+	"image/color"
+	"log"
 )
 
-// Game object used by ebiten
 type game struct {
 	ui           *ebitenui.UI
 	textInputs   []*widget.TextInput // Хранение ссылок на текстовые поля
 	updateButton *widget.Button      // Кнопка "Update Text"
+	inMainMenu   bool
 }
 
-type ListEntry struct {
-	id   int
-	name string
+var face, _ = loadFont(20)
+
+var InputFields = make(map[string]*widget.TextInput)
+
+func NewLabel(labelText string) *widget.Label {
+	return widget.NewLabel(
+		widget.LabelOpts.Text(labelText, face, &widget.LabelColor{
+			Idle: color.White,
+		}),
+		widget.LabelOpts.TextOpts(
+			widget.TextOpts.Position(widget.TextPositionEnd, widget.TextPositionEnd),
+			widget.TextOpts.WidgetOpts(
+				widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+					Position: widget.RowLayoutPositionEnd,
+				}),
+			),
+		),
+	)
 }
 
-func main() {
-	ebiten.SetWindowSize(400, 600)
-	ebiten.SetWindowTitle("Ebiten UI - TextInput with Labels")
+func AddInputFieldWithText(container *widget.Container, labelText string, placeholder string) {
+	label := NewLabel(labelText)
 
-	buttonImage, _ := loadButtonImage()
+	textInput := widget.NewTextInput(
+		widget.TextInputOpts.WidgetOpts(
+			widget.WidgetOpts.MinSize(200, 30),
+		),
+		widget.TextInputOpts.Image(&widget.TextInputImage{
+			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
+			Disabled: image.NewNineSliceColor(color.NRGBA{R: 50, G: 50, B: 50, A: 255}),
+		}),
+		widget.TextInputOpts.Face(face),
+		widget.TextInputOpts.Color(&widget.TextInputColor{
+			Idle:          color.NRGBA{R: 254, G: 255, B: 255, A: 255},
+			Disabled:      color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+			Caret:         color.NRGBA{R: 254, G: 255, B: 255, A: 255},
+			DisabledCaret: color.NRGBA{R: 200, G: 200, B: 200, A: 255},
+		}),
+		widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
+		widget.TextInputOpts.CaretOpts(widget.CaretOpts.Size(face, 2)),
+		widget.TextInputOpts.Placeholder(placeholder),
+	)
 
-	// Создаем объект игры
-	game := &game{}
+	InputFields[labelText] = textInput
+	container.AddChild(label)
+	container.AddChild(textInput)
 
-	// Загружаем шрифт
-	face, _ := loadFont(20)
+}
 
-	// Создаем корневой контейнер с макетом GridLayout
-	rootContainer := widget.NewContainer(
+func NewCenterContainer() *widget.Container {
+	return widget.NewContainer(
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{R: 0x13, G: 0x1a, B: 0x22, A: 0xff})),
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout(
+			widget.AnchorLayoutOpts.Padding(widget.NewInsetsSimple(30)),
+		)),
+	)
+}
+
+func NewGridContainer() *widget.Container {
+	return widget.NewContainer(
 		widget.ContainerOpts.Layout(widget.NewGridLayout(
 			widget.GridLayoutOpts.Columns(2),                         // 2 колонки: для метки и текстового поля
 			widget.GridLayoutOpts.Stretch([]bool{false, false}, nil), // Метка фиксирована, поле растягивается
 			widget.GridLayoutOpts.Spacing(10, 10),
 		)),
-		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{0x13, 0x1a, 0x22, 0xff})),
+		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{R: 0x13, G: 0x1a, B: 0x22, A: 0xff})),
+		widget.ContainerOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+				StretchHorizontal:  true,
+				StretchVertical:    false,
+			})),
 	)
+}
 
-	// Функция для добавления метки и текстового поля
-	addTextInputWithLabel := func(labelText string, placeholder string, validationFunc func(string) (bool, *string)) *widget.TextInput {
-		// Метка
-		label := widget.NewLabel(
-			widget.LabelOpts.Text(labelText, face, &widget.LabelColor{
-				Idle: color.White,
-			}),
-			widget.LabelOpts.TextOpts(
-				widget.TextOpts.Position(widget.TextPositionCenter, widget.TextPositionCenter),
-				widget.TextOpts.WidgetOpts(
-					widget.WidgetOpts.LayoutData(widget.RowLayoutData{
-						Position: widget.RowLayoutPositionCenter,
-					}),
-				),
-			),
-		)
+func AddComboboxFieldWithText(container *widget.Container, labelText string, entries []any,
+	selectHandler func(args *widget.ListComboButtonEntrySelectedEventArgs)) {
+	buttonImage, _ := loadButtonImage()
 
-		// Текстовое поле
-		textInput := widget.NewTextInput(
-			widget.TextInputOpts.WidgetOpts(
-				widget.WidgetOpts.MinSize(200, 30),
-			),
-			widget.TextInputOpts.Image(&widget.TextInputImage{
-				Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 100, A: 255}),
-				Disabled: image.NewNineSliceColor(color.NRGBA{R: 50, G: 50, B: 50, A: 255}),
-			}),
-			widget.TextInputOpts.Face(face),
-			widget.TextInputOpts.Color(&widget.TextInputColor{
-				Idle:          color.NRGBA{254, 255, 255, 255},
-				Disabled:      color.NRGBA{R: 200, G: 200, B: 200, A: 255},
-				Caret:         color.NRGBA{254, 255, 255, 255},
-				DisabledCaret: color.NRGBA{R: 200, G: 200, B: 200, A: 255},
-			}),
-			widget.TextInputOpts.Padding(widget.NewInsetsSimple(5)),
-			widget.TextInputOpts.CaretOpts(widget.CaretOpts.Size(face, 2)),
-			widget.TextInputOpts.Placeholder(placeholder),
-			widget.TextInputOpts.Validation(validationFunc),
-			widget.TextInputOpts.ChangedHandler(func(args *widget.TextInputChangedEventArgs) {
-				fmt.Println("Text Changed: ", args.InputText)
-			}),
-		)
-
-		// Добавляем метку и поле в контейнер
-		rootContainer.AddChild(label)
-		rootContainer.AddChild(textInput)
-
-		return textInput
-	}
-
-	// Добавляем поля с метками
-	game.textInputs = append(game.textInputs, addTextInputWithLabel("Hello World", "Standard Textbox", nil))
-	game.textInputs = append(game.textInputs, addTextInputWithLabel("Hello World", "Password Textbox", nil))
-	game.textInputs = append(game.textInputs, addTextInputWithLabel("Hello World", "Max length (5)", func(newInputText string) (bool, *string) {
-		if utf8.RuneCountInString(newInputText) > 5 {
-			return false, nil
-		}
-		return true, nil
-	}))
-	game.textInputs = append(game.textInputs, addTextInputWithLabel("Hello World", "All Caps", func(newInputText string) (bool, *string) {
-		newInputText = strings.ToUpper(newInputText)
-		return false, &newInputText
-	}))
-
-	numEntries := 20
-	entries := make([]any, 0, numEntries)
-	for i := 1; i <= numEntries; i++ {
-		entries = append(entries, ListEntry{i, fmt.Sprintf("Entry %d", i)})
-	}
-	// construct a combobox
 	comboBox := widget.NewListComboButton(
 		widget.ListComboButtonOpts.SelectComboButtonOpts(
 			widget.SelectComboButtonOpts.ComboButtonOpts(
-				//Set the max height of the dropdown list
 				widget.ComboButtonOpts.MaxContentHeight(150),
-				//Set the parameters for the primary displayed button
 				widget.ComboButtonOpts.ButtonOpts(
 					widget.ButtonOpts.Image(buttonImage),
 					widget.ButtonOpts.TextPadding(widget.NewInsetsSimple(5)),
@@ -134,23 +113,15 @@ func main() {
 						Disabled: color.White,
 					}),
 					widget.ButtonOpts.WidgetOpts(
-						//Set how wide the button should be
 						widget.WidgetOpts.MinSize(150, 0),
-						//Set the combobox's position
-						widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
-							HorizontalPosition: widget.AnchorLayoutPositionCenter,
-							VerticalPosition:   widget.AnchorLayoutPositionCenter,
-						})),
+					),
 				),
 			),
 		),
 		widget.ListComboButtonOpts.ListOpts(
-			//Set how wide the dropdown list should be
 			widget.ListOpts.ContainerOpts(widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.MinSize(150, 0))),
-			//Set the entries in the list
 			widget.ListOpts.Entries(entries),
 			widget.ListOpts.ScrollContainerOpts(
-				//Set the background images/color for the dropdown list
 				widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
 					Idle:     image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
 					Disabled: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
@@ -158,17 +129,13 @@ func main() {
 				}),
 			),
 			widget.ListOpts.SliderOpts(
-				//Set the background images/color for the background of the slider track
 				widget.SliderOpts.Images(&widget.SliderTrackImage{
 					Idle:  image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
 					Hover: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
 				}, buttonImage),
 				widget.SliderOpts.MinHandleSize(5),
-				//Set how wide the track should be
 				widget.SliderOpts.TrackPadding(widget.NewInsetsSimple(2))),
-			//Set the font for the list options
 			widget.ListOpts.EntryFontFace(face),
-			//Set the colors for the list
 			widget.ListOpts.EntryColor(&widget.ListEntryColor{
 				Selected:                   color.NRGBA{254, 255, 255, 255},             //Foreground color for the unfocused selected entry
 				Unselected:                 color.NRGBA{254, 255, 255, 255},             //Foreground color for the unfocused unselected entry
@@ -179,89 +146,88 @@ func main() {
 				DisabledSelected:           color.NRGBA{100, 100, 100, 255},             //Foreground color for the disabled selected entry
 				DisabledSelectedBackground: color.NRGBA{100, 100, 100, 255},             //Background color for the disabled selected entry
 			}),
-			//Padding for each entry
 			widget.ListOpts.EntryTextPadding(widget.NewInsetsSimple(5)),
 		),
-		//Define how the entry is displayed
 		widget.ListComboButtonOpts.EntryLabelFunc(
 			func(e any) string {
-				//Button Label function
-				return e.(ListEntry).name
+				return e.(string)
 			},
 			func(e any) string {
-				//List Label function
-				return "List: " + e.(ListEntry).name
+				return e.(string)
 			}),
-		//Callback when a new entry is selected
-		widget.ListComboButtonOpts.EntrySelectedHandler(func(args *widget.ListComboButtonEntrySelectedEventArgs) {
-			fmt.Println("Selected Entry: ", args.Entry)
-		}),
+		widget.ListComboButtonOpts.EntrySelectedHandler(selectHandler),
 	)
-	label := widget.NewLabel(
-		widget.LabelOpts.Text("DISTANC", face, &widget.LabelColor{
-			Idle: color.White,
+
+	label := NewLabel(labelText)
+
+	container.AddChild(label)
+	container.AddChild(comboBox)
+}
+
+func NewButton(buttonText string, clickHandler func(args *widget.ButtonClickedEventArgs)) *widget.Button {
+	return widget.NewButton(
+		widget.ButtonOpts.Text(buttonText, face, &widget.ButtonTextColor{
+			Idle: color.NRGBA{R: 0xdf, G: 0xf4, B: 0xff, A: 0xff},
 		}),
-		widget.LabelOpts.TextOpts(
-			widget.TextOpts.Position(widget.TextPositionCenter, widget.TextPositionCenter),
-			widget.TextOpts.WidgetOpts(
-				widget.WidgetOpts.LayoutData(widget.RowLayoutData{
-					Position: widget.RowLayoutPositionCenter,
-				}),
-			),
+		widget.ButtonOpts.Image(&widget.ButtonImage{
+			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 150, B: 200, A: 255}),
+			Hover:    image.NewNineSliceColor(color.NRGBA{R: 120, G: 170, B: 220, A: 255}),
+			Pressed:  image.NewNineSliceColor(color.NRGBA{R: 80, G: 120, B: 180, A: 255}),
+			Disabled: image.NewNineSliceColor(color.NRGBA{R: 50, G: 50, B: 50, A: 255}),
+		}),
+		widget.ButtonOpts.WidgetOpts(
+			widget.WidgetOpts.MinSize(150, 35),
 		),
+		widget.ButtonOpts.ClickedHandler(clickHandler),
 	)
+}
 
-	rootContainer.AddChild(label)
-	rootContainer.AddChild(comboBox)
+func main() {
 
-	// Создаем кнопку "Update Text"
-	game.updateButton = widget.NewButton(
-		widget.ButtonOpts.Text("Update Text", face, &widget.ButtonTextColor{
-			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
-		}),
-		widget.ButtonOpts.Image(&widget.ButtonImage{
-			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 150, B: 200, A: 255}),
-			Hover:    image.NewNineSliceColor(color.NRGBA{R: 120, G: 170, B: 220, A: 255}),
-			Pressed:  image.NewNineSliceColor(color.NRGBA{R: 80, G: 120, B: 180, A: 255}),
-			Disabled: image.NewNineSliceColor(color.NRGBA{R: 50, G: 50, B: 50, A: 255}),
-		}),
-		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
-			// Сброс всех текстовых полей
-			for _, input := range game.textInputs {
-				input.SetText("")
-			}
-		}),
-	)
+	ebiten.SetWindowSize(400, 400)
+	ebiten.SetWindowTitle("Ebiten UI - TextInput with Labels")
 
-	runButton := widget.NewButton(
-		widget.ButtonOpts.Text("Run", face, &widget.ButtonTextColor{
-			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
-		}),
-		widget.ButtonOpts.Image(&widget.ButtonImage{
-			Idle:     image.NewNineSliceColor(color.NRGBA{R: 100, G: 150, B: 200, A: 255}),
-			Hover:    image.NewNineSliceColor(color.NRGBA{R: 120, G: 170, B: 220, A: 255}),
-			Pressed:  image.NewNineSliceColor(color.NRGBA{R: 80, G: 120, B: 180, A: 255}),
-			Disabled: image.NewNineSliceColor(color.NRGBA{R: 50, G: 50, B: 50, A: 255}),
-		}),
-		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
-			// Сброс всех текстовых полей
-			for _, input := range game.textInputs {
-				input.SetText("")
-			}
-		}),
-	)
+	game := &game{inMainMenu: true}
 
-	// Добавляем кнопку в правый нижний угол
-	rootContainer.AddChild(game.updateButton)
-	rootContainer.AddChild(runButton)
+	rootContainer := NewCenterContainer()
+	innerContainer := NewGridContainer()
 
-	// Создаем UI
+	AddInputFieldWithText(innerContainer, "Count cats", "integer number")
+	AddInputFieldWithText(innerContainer, "Update time", "time in second")
+	AddInputFieldWithText(innerContainer, "Angry radius", "in pixels")
+	AddInputFieldWithText(innerContainer, "Hissing radius", "in pixels")
+
+	AddComboboxFieldWithText(innerContainer, "Distance type", []any{"Euclidean", "Manhattan", "Curvilinear"}, func(args *widget.ListComboButtonEntrySelectedEventArgs) {
+		fmt.Println(args.Entry)
+	})
+
+	AddComboboxFieldWithText(innerContainer, "Geometry type", []any{"Simple", "Vector"}, func(args *widget.ListComboButtonEntrySelectedEventArgs) {
+		fmt.Println(args.Entry)
+	})
+
+	resetButton := NewButton("Reset", func(args *widget.ButtonClickedEventArgs) {
+		for _, input := range InputFields {
+			input.SetText("")
+		}
+	})
+
+	runButton := NewButton("Run", func(args *widget.ButtonClickedEventArgs) {
+		game.inMainMenu = false
+		ebiten.SetFullscreen(true)
+
+		// TODO validate appConfig HERE
+	})
+
+	innerContainer.AddChild(resetButton)
+	innerContainer.AddChild(runButton)
+
+	rootContainer.AddChild(innerContainer)
+
 	ui := ebitenui.UI{
 		Container: rootContainer,
 	}
 	game.ui = &ui
 
-	// Запускаем игру
 	err := ebiten.RunGame(game)
 	if err != nil {
 		log.Println(err)
@@ -275,24 +241,33 @@ func (g *game) Layout(outsideWidth int, outsideHeight int) (int, int) {
 
 // Update implements Game.
 func (g *game) Update() error {
-	// Обновление UI
-	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
-		g.ui.ChangeFocus(widget.FOCUS_PREVIOUS)
+
+	if g.inMainMenu {
+		// Обновление UI
+		if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+			g.ui.ChangeFocus(widget.FOCUS_PREVIOUS)
+		}
+		if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+			g.ui.ChangeFocus(widget.FOCUS_NEXT)
+		}
+		g.ui.Update()
+	} else {
+		//g.ui.Update()
 	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
-		g.ui.ChangeFocus(widget.FOCUS_NEXT)
-	}
-	g.ui.Update()
 	return nil
 }
 
 // Draw implements Ebiten's Draw method.
 func (g *game) Draw(screen *ebiten.Image) {
 	// Рисуем UI
-	g.ui.Draw(screen)
+	//fmt.Println(g.inMainMenu)
+	if g.inMainMenu {
+		g.ui.Draw(screen)
+	} else {
+		screen.Fill(color.NRGBA{R: 100, G: 150, B: 200, A: 255})
+	}
 }
 
-// Загрузка шрифта
 func loadFont(size float64) (text.Face, error) {
 	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
 	if err != nil {
