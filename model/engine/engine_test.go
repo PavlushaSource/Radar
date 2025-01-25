955a4b22128a062927f6c77fdb2c726850bd17f3
PavlushaSource <pavlushkin.dima1785@gmail.com>
2025-01-25 14:46:11 +0300

    feat: Add TestTable for performance

--- model/engine/engine_test.go
+++ model/engine/engine_test.go
@@ -2,35 +2,55 @@ package engine
 
 import (
 	"context"
+	"fmt"
 	"github.com/PavlushaSource/Radar/model/core/rnd"
 	"github.com/PavlushaSource/Radar/model/geom"
 	"testing"
 	"time"
 )
 
+var inputPerformanceTest = []struct {
+	winX, winY      float64
+	maxMoveDistance float64
+	dist            geom.Distance
+	rFight, rHiss   float64
+}{
+	{1920, 1080, 300, geom.ManhattanDistance, 15, 30},
+	{640, 640, 50, geom.EuclideanDistance, 8, 15},
+	{1920, 1080, 75, geom.CurvilinearDistance, 16, 32},
+}
+
 func TestPerformance(t *testing.T) {
-	rndAsync := rnd.NewRndCore()
-	g := geom.NewSimpleGeom(1080, 1920, make([]geom.Barrier, 0), 100, geom.EuclideanDistance, rndAsync)
-	eng := NewEngine(15, 30, 50000, g, rndAsync, 5)
 
-	getCh, putCh := eng.Run(context.Background())
+	for i, tt := range inputPerformanceTest {
 
-	start := time.Now()
-	iterationCount := 0
-	for state := range getCh {
-		end := time.Now()
+		name := fmt.Sprintf("Test %d", i)
+		t.Run(name, func(t *testing.T) {
+			rndAsync := rnd.NewRndCore()
+			g := geom.NewSimpleGeom(tt.winY, tt.winX, make([]geom.Barrier, 0), tt.maxMoveDistance, tt.dist, rndAsync)
+			eng := NewEngine(tt.rFight, tt.rHiss, 50000, g, rndAsync, 5)
 
-		putCh <- state
-		spend := end.Sub(start)
-		if spend > 250*time.Millisecond {
-			t.Errorf("spend too long (%v)", spend)
-		}
+			getCh, putCh := eng.Run(context.Background())
 
-		if iterationCount >= 5 {
-			return
-		}
-		iterationCount++
+			start := time.Now()
+			iterationCount := 0
+			for state := range getCh {
+				end := time.Now()
 
-		start = time.Now()
+				putCh <- state
+				spend := end.Sub(start)
+				if spend > 500*time.Millisecond {
+					t.Errorf("spend too long (%v)", spend)
+				}
+
+				if iterationCount >= 5 {
+					return
+				}
+				iterationCount++
+
+				start = time.Now()
+			}
+		})
 	}
+
 }
